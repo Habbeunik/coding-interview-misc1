205ae2ec72b16d5a234703d4dfb8f8b8cd9eb8b0
habbey <habbeyunik@gmail.com>
2021-12-06 22:48:46 +0100

    Attempt light dark mode

--- src/LightDarkMode.tsx
+++ src/LightDarkMode.tsx
@@ -1,35 +1,38 @@
-import React from 'react';
+import React, { useState, createContext, useContext } from "react";
+
+enum MODES {
+  DARK = "dark",
+  LIGHT = "light",
+}
+
+const ThemeModeContext = createContext({
+  themeMode: MODES.LIGHT,
+  setThemeMode: (mode: MODES) => {},
+});
 
 export const App = () => {
-
-    return (
-        <Main/>
-    );
-
-}
-
+  const [themeMode, setThemeMode] = useState(MODES.LIGHT);
+  return (
+    <ThemeModeContext.Provider value={{ themeMode, setThemeMode }}>
+      <Main />
+    </ThemeModeContext.Provider>
+  );
+};
 
 export const Main = () => {
-
-    return (
-        <div>
-            <Settings/>
-        </div>
-    );
-
-}
+  return (
+    <div>
+      <Settings />
+    </div>
+  );
+};
 
 export const Settings = () => {
+  const { themeMode, setThemeMode } = useContext(ThemeModeContext);
 
-    const toggleMode = React.useCallback(() => {
+  const toggleMode = React.useCallback(() => {
+    setThemeMode(themeMode === MODES.DARK ? MODES.LIGHT : MODES.DARK);
+  }, [themeMode, setThemeMode]);
 
-    }, []);
-
-    return (
-        <button onClick={toggleMode}>toggle light/dark mode</button>
-    );
-
-}
-
-
-
+  return <button onClick={toggleMode}>toggle light/dark mode</button>;
+};
